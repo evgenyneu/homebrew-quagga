3788b71d81e885b281cb057b30aacaac3756a993
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2024-10-17 15:24:59 +1100

    Release new version

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Homebrew tap for [Quagga](https://github.com/evgenyneu/quagga), a command-line u
 brew tap evgenyneu/quagga
 brew install quagga
 ```
+
+## How to release a new version
+
+* Open [quagga.rb](quagga.rb).
+* Update the version.
+* Update the SHA256 hashes by downloading the files from Github releases (https://github.com/evgenyneu/quagga/releases) and calculate the hash for each binary:
+
+```
+shasum -a 256 quagga-macos-v0.1.3.tar.gz
+```
--- quagga.rb
+++ quagga.rb
@@ -1,21 +1,21 @@
 class Quagga < Formula
   desc "A command-line utility that combines multiple text files into a single prompt suitable for Large Language Models."
   homepage "https://github.com/evgenyneu/quagga"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/evgenyneu/quagga/releases/download/v#{version}/quagga-macos-arm64.tar.gz"
-      sha256 "7fe64755ba77de392f50f77e0c243bfa5f10f2f636f8e803bd4cddb21b0e39c1"
+      sha256 "88648a1848ad0bc0f75241389d1fb87311c16036ffe17f03c60209e73984ff53"
     else
       url "https://github.com/evgenyneu/quagga/releases/download/v#{version}/quagga-macos-amd64.tar.gz"
-      sha256 "018a1c7d7900523b14fb3343285a41040eb87a1ab681bf860d9f0178b10a9af0"
+      sha256 "a08066bf2926d5a01b4843342736541e1fd07238638e6e2e866efcc413cb5d7e"
     end
   end
 
   on_linux do
     url "https://github.com/evgenyneu/quagga/releases/download/v#{version}/quagga-linux-amd64.tar.gz"
-    sha256 "f751cefaaa01385e9b37697d80e3195b662830e76c0209772d3737ebf3621f81"
+    sha256 "9c35985a8ad7b01d8970e9b0a878c8eae969d4ea0c1a8bce1336a9f9f28beff4"
   end
 
   def install
